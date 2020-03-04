5d7bd41e0967b8754d57aa6ec88b3df41bf786a0
Adrian <mohabiradrian@gmail.com>
2020-03-04 15:14:04 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ApplicationRecord
-    validates :title, presence: true, uniqueness: true 
+    validates :title, presence: true, uniqueness: {scope: :release_year}
     validates :released, inclusion: {in: [true, false]}, acceptance: true 
     validates :artist_name, presence: true 
 end
