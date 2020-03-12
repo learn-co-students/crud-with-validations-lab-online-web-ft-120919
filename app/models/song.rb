8705bd59210c3bc4f5a705515a3949c1f650b159
mrarthurwhite <mister.arthur.white@gmail.com>
2020-03-12 16:51:43 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,35 @@
+class Song < ApplicationRecord
+  validates :title, presence: true
+  validates :released, inclusion: { in: %w(true false)}
+  validates :artist_name, presence: true
+
+  validate :title_artist_year_validation
+  validate :release_year_validation
+
+  def title_artist_year_validation
+    # title cannot be repeated by the same artist in the same year
+    song = Song.find_by title: :title , artist_name: :artist_name,  release_year: :release_year
+    if !song==nil
+      errors.add(:title, "Title cannot be repeated by the same artist in the same year")
+    end
+  end
+
+
+=begin
+     Optional if released is false
+     Must not be blank if released is true
+     Must be less than or equal to the current year
+=end
+  def release_year_validation
+    if(released)
+      if !release_year.present?
+        errors.add(:release_year, "Release Year required for released songs.")
+        if (release_year >(Date.today.year))
+          errors.add(:release_year, "Release Year must NOT be >=  current year")
+        end
+      end
+    end
+  end
+
+
+end
