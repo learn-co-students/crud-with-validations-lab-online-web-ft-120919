3d6e447796026bd4d84811f0fbfe4823fc6890d1
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-10-23 11:49:54 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,14 +3,49 @@ class SongsController < ApplicationController
         @songs = Song.all
     end
 
+    def show
+        @song = Song.find(params[:id])
+    end
+
+
     def new
         @song = Song.new
+    end
+
+    def create
+        @song = Song.new(song_params)
+         if @song.save
+            redirect_to song_path(@song)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @song = Song.find(params[:id])
+    end
+
+    def update
+        @song = Song.find(params[:id])
+        @song.update(song_params)
+        if @song.save
+            redirect_to song_path(@song)
+        end
+    end
+
+    def destroy
+        @song = Song.find(params[:id])
+        if @song.destroy
+        redirect_to @songs_path
+        else
+            render @song
+        end
     end
 
 
     private
 
     def song_params
-        params.require(@song)
+        params.require(:song).permit(:title, :artist_name, :released, :genre, :release_year)
     end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes) do
+    {
+      title: "Parachutes",
+      artist_name: "Balance and Composure",
+      release_year: 2015,
+      released: true,
+      genre: "Post hardcore"
+    }
+  end
+
+
 end
